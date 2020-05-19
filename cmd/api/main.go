9cc29f82f81c4b4b7ecbf18b686761248241ec90
slavamuravey <slavamuravey@mail.ru>
2020-05-19 09:15:00 +0700

    Add jsonp example

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,5 +8,21 @@ import (
 
 func main() {
 	http.HandleFunc("/api", handler.Handler)
+	http.HandleFunc("/jsonp", jsonpHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func jsonpHandler(w http.ResponseWriter, r *http.Request) {
+	callback := r.URL.Query().Get("callback")
+	switch callback {
+	case "gotWeather":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`gotWeather({
+  temperature: 25,
+  humidity: 78
+});
+`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
