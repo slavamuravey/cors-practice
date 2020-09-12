673be1eaa27992b84510b39838107e3bdc2a5cc6
slavamuravey <slavamuravey@mail.ru>
2020-09-12 17:03:58 +0700

    Add examples

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"github.com/slavamuravey/cors"
 	"github.com/slavamuravey/cors-practice/pkg/handler"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/api", handler.Handler)
+	config := &cors.Config{
+		AllowAllOrigin: true,
+	}
+
+	corsMiddleware := cors.CreateMiddleware(config)
+
+	http.HandleFunc("/api-no-cors", handler.Handler)
+	http.HandleFunc("/api-with-cors", corsMiddleware(http.HandlerFunc(handler.Handler)))
+
+	config1 := &cors.Config{
+		AllowCredentials:           false,
+		AllowOriginPattern:         "http://127.0.0.1:8181",
+	}
+
+	corsMiddleware1 := cors.CreateMiddleware(config1)
+	http.HandleFunc("/api-with-cors-with-credentials-error", corsMiddleware1(http.HandlerFunc(handler.Handler)))
+
+	config2 := &cors.Config{
+		AllowCredentials:           true,
+		AllowOriginPattern:         "http://127.0.0.1:8181",
+	}
+
+	corsMiddleware2 := cors.CreateMiddleware(config2)
+	http.HandleFunc("/api-with-cors-with-credentials-success", corsMiddleware2(http.HandlerFunc(handler.Handler)))
+
+	config3 := &cors.Config{
+		AllowCredentials:           true,
+		AllowAllOrigin: true,
+	}
+
+	corsMiddleware3 := cors.CreateMiddleware(config3)
+	http.HandleFunc("/api-with-cors-with-credentials-wildcard-error", corsMiddleware3(http.HandlerFunc(handler.Handler)))
+
 	http.HandleFunc("/jsonp", jsonpHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/slavamuravey/cors-practice
 
 go 1.13
+
+require github.com/slavamuravey/cors v1.0.2
